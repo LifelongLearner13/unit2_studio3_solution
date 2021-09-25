d1c5540ec5c66486b9f36b55b69783ca1bb8ca10
lynn <sierragregg@Gmail.com>
2021-09-25 13:36:49 -0500

    init

---
+++ unit2_studio3_solution/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace unit2_studio3_solution
+{
+    public class Menu
+    {
+        public DateTime LastUpdated { get; }
+        public List<MenuItem> Items { get; set; }
+
+        public Menu(DateTime date, List<MenuItem> items)
+        {
+            LastUpdated = date;
+            Items = items;
+        }
+
+        public Menu(List<MenuItem> items): this(new DateTime(), items) { }
+    }
+}
---
+++ unit2_studio3_solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace unit2_studio3_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
